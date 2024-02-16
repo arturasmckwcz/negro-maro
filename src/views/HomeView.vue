5717e4aa6ae1c6ce557c540cd57ecc783c159075
Arturas Mickiewicz <57557793+arturasmckwcz@users.noreply.github.com>
2024-02-16 12:56:33 +0100

    got rid of intermediate component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,35 @@
 <template>
   <SplashComponent />
-  <RecommendedComponent
-    :inventory="inventory"
-    :recommended="recommended"
-    :addToCart="addToCart"
-  />
+  <main v-if="true" class="wrapper">
+    <h2>Recommended</h2>
+
+    <div class="recommended">
+      <CardComponent
+        class="card"
+        v-for="product in cards"
+        :product="product"
+        :key="product.id"
+        :addToCart="addToCart"
+      ></CardComponent>
+    </div>
+  </main>
 </template>
 
 <script>
 import SplashComponent from "@/components/SplashComponent.vue";
-import RecommendedComponent from "@/components/RecommendedComponent.vue";
+import CardComponent from "@/components/CardComponent.vue";
 
 export default {
   name: "HomeView",
-  components: { SplashComponent, RecommendedComponent },
+  components: { SplashComponent, CardComponent },
   props: ["inventory", "recommended", "addToCart"],
+  computed: {
+    cards() {
+      const cards = this.inventory.filter(({ id }) =>
+        this.recommended.includes(id)
+      );
+      return cards;
+    },
+  },
 };
 </script>
